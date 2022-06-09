bc4e8fc6790cf6af9de6f7b456505cf19a081afa
Miller Juma <miller.developer.william@gmail.com>
2022-06-09 19:29:43 +0300

    routes

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use \App\Http\Controllers\API\Fleet\FleetController;
+use \App\Http\Controllers\API\Customer\CustomerController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::apiResource('fleet', FleetController::class);
+Route::apiResource('customers', CustomerController::class);
