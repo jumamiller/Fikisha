c682dbfc30819081910ca542fc33fc5d8ad1a3f8
Miller Juma <miller.developer.william@gmail.com>
2022-06-09 18:44:33 +0300

    notification

--- app/Notifications/OrderDispatchedNotification.php
+++ app/Notifications/OrderDispatchedNotification.php
@@ -41,9 +41,8 @@ class OrderDispatchedNotification extends Notification
     public function toMail($notifiable)
     {
         return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
-                    ->line('Thank you for using our application!');
+                    ->subject('Order Dispatched')
+                    ->line('Hi, your order has been dispatched!');
     }
 
     /**
